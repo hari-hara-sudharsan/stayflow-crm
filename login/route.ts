0ba0eda4033fc08dc0c0853588f58c57a251941c
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-05-01 22:34:08 +0530

    Created login route file

---
+++ login/route.ts
@@ -0,0 +1,54 @@
+import { connectDB } from "@/lib/db";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    // 🔎 Basic validation
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+
+    await connectDB();
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User not found" },
+        { status: 400 }
+      );
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { error: "Invalid password" },
+        { status: 400 }
+      );
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET || "secret_key",
+      { expiresIn: "1d" }
+    );
+
+    return NextResponse.json({ token });
+  } catch (err) {
+    console.error("LOGIN ERROR:", err);
+
+    return NextResponse.json(
+      { error: "Server error" },
+      { status: 500 }
+    );
+  }
+}
